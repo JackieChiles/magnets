0a76b9a16819ed65d5eabf97bef6a0d47c57833d
JackieChiles <dtgaynor@gmail.com>
2015-12-23 21:57:01 -0500

    Added visit maps.

--- public/js/directives.js
+++ public/js/directives.js
@@ -7,3 +7,28 @@ app.directive('magVisit', function () {
 	}
     };
 });
+
+app.directive('magVisitMap', function () {
+    return {
+	restrict: 'E',
+	scope: {
+	    visit: '='
+	},
+	link: function (scope, element, attrs) {
+	    element.css({
+		width: '300px',
+		height: '200px',
+		display: 'block'
+	    });
+
+	    var map = L.map(element[0], {
+		center: [scope.visit.lat, scope.visit.long],
+		zoom: 12
+	    });
+
+	    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	    }).addTo(map);
+	}
+    };
+});
